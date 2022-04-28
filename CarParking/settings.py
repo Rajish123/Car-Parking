da7ca990bbd573763860a1d9db69fb828e2b92a9
rajish <maharjanrajish101@gmail.com>
2022-04-28 10:51:51 +0545

    Removed Secret key

--- CarParking/settings.py
+++ CarParking/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&6ay-(%$v2@n2@zsno!xx9)-r7(tdi+jr8sqhrgm+pn1kgga3g'
-
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
